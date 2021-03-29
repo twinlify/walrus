a744b1cb36c388a5254de0fea97b456c259655d3
chrisdugne <chris.dugne@uralys.com>
2021-03-29 21:12:44 +0200

    removed unused des, added globalStyle

---
+++ src/style/global-style.js
@@ -0,0 +1,20 @@
+import {primary} from './colors';
+
+export const globalStyle = `
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #ededed;
+  font-size: 17px;
+  font-family: Poppins;
+}
+
+a {
+  color: ${primary};
+  font-weight: 500;
+  text-decoration: none;
+
+  &:hover {
+    color: #444;
+  }
+}`;
